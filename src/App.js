45454efb7cfc486b0208d63d5551e83e8c72de3a
Dhivyaa <dhivyaa12122001@gmail.com>
2021-10-15 11:25:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  Card, CardBody
+} from 'reactstrap';
+import Emoji from './emoji.json'
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [datas, setData] = useState(Emoji);
+  
+  useEffect(()=>{
+      const emojiResult = Emoji.filter((emo) => emo.title.toLowerCase().includes(search.toLowerCase()));
+      setData(emojiResult);
+  },[search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="emoji_title">Emoji Search</div>
+        <input type="text" placeholder="Search Emojis" value={search} onChange={(e) => setSearch(e.target.value)}/>
+      </div>
+      <div className="cards">
+        { 
+          datas.length===0?
+          <div className="results">No Emojis Found</div>:
+          datas.map((data) =>{
+            return(
+              <div className="cardy">
+                <Card>
+                  <CardBody>
+                    <div className="title">{data.title}</div>
+                    <div className="symbol">{data.symbol}</div>
+                    <div className="keywords">{data.keywords}</div>
+                  </CardBody>
+                </Card>
+              </div>
+              )
+            })
+        }
+      </div>
     </div>
   );
 }
